ec3e021f346f1ccf23193a5fa035351f8861b855
West Wang <west@misfit.com>
2015-07-04 22:36:05 +0800

    fix infer

--- scripts/install_infer.sh
+++ scripts/install_infer.sh
@@ -2,6 +2,6 @@
 
 find $INFER_HOME -name infer
 
-if [[ -f $INFER_HOME/infer/infer/bin/infer ]]; then
+if [[ -f $INFER_HOME/infer/bin/infer ]]; then
     git clone --depth=50 https://github.com/facebook/infer.git $INFER_HOME
 fi
