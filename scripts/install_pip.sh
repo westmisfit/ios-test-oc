e5a37f51d32947000d367f6655940b3b067dcae2
West Wang <west@misfit.com>
2015-07-04 23:18:35 +0800

    fix install pip

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-function install_pip
-{
-    # echo easy_install pip
-    curl -O https://bootstrap.pypa.io/get-pip.py
-    python get-pip.py
-}
-
-which pip && install_pip
+which pip > /dev/null || curl -O https://bootstrap.pypa.io/get-pip.py && python get-pip.py
