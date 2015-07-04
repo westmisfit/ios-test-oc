dd3e0aadb695219882cb1c20a78c6b479dcaa359
West Wang <west@misfit.com>
2015-07-04 22:32:50 +0800

    fix infer

---
+++ scripts/install_infer.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+find $INFER_HOME -name infer
+
+if [[ -f $INFER_HOME/infer/infer/bin/infer ]]; then
+    git clone --depth=50 https://github.com/facebook/infer.git $INFER_HOME
+fi
---
+++ scripts/install_pip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [[ which pip ]]; then
+    echo easy_install pip
+fi
