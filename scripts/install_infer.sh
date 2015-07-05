ff2930e203e2518e3fb075586b27523252561585
West Wang <west@misfit.com>
2015-07-05 16:15:58 +0800

    fix install infer

--- scripts/install_infer.sh
+++ scripts/install_infer.sh
@@ -5,9 +5,11 @@ INFER_HOME="${INFER_HOME-$HOME/infer}"
 mkdir -p $INFER_HOME/infer
 
 if [[ ! -f $INFER_HOME/infer/bin/infer ]]; then
-    git clone --depth=50 git@github.com:facebook/infer.git $INFER_HOME/infer
-    ls -al $INFER_HOME/infer
+    git clone --depth=50 https://github.com/facebook/infer.git $INFER_HOME/infer
+    cd $INFER_HOME/infer
+    git checkout v0.2.0
     $INFER_HOME/infer/update-fcp.sh
 fi
 
-find -name infer
+# find $INFER_HOME/infer -name infer
+which infer
