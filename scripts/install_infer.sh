5d37250737a90e94df8ea7590ae786d3c1eec8fa
West Wang <west@misfit.com>
2015-07-05 17:41:52 +0800

    modify install_infer script

--- scripts/install_infer.sh
+++ scripts/install_infer.sh
@@ -5,12 +5,12 @@ INFER_VERSION=0.1.1
 
 mkdir -p $INFER_HOME
 
-if [[ ! -f $INFER_HOME/infer-${INFER_VERSION}/infer/bin/infer ]]; then
+if [[ ! -f $INFER_HOME/infer/infer/bin/infer ]]; then
     cd $INFER_HOME
-    curl -L -o infer-0.1.1.tar.gz https://github.com/facebook/infer/archive/v0.1.1.tar.gz
-    tar xzf infer-0.1.1.tar.gz
+    curl -L -o infer-${INFER_VERSION}.tar.gz https://github.com/facebook/infer/archive/v${INFER_VERSION}.tar.gz
+    tar xzf infer-${INFER_VERSION}.tar.gz
     ls -al
-    mv infer-0.1.1 infer
+    mv infer-${INFER_VERSION} infer
     cd infer
     ls -al
     ls -al infer/bin/
