2c2c0777455141993077dcfa4a2f6eeb200b5189
West Wang <west@misfit.com>
2015-07-05 14:18:50 +0800

    fix install infer

--- scripts/install_infer.sh
+++ scripts/install_infer.sh
@@ -1,5 +1,10 @@
 set -ex
 if [[ ! -f $INFER_HOME/infer/bin/infer ]]; then
-    git clone --depth=50 https://github.com/facebook/infer.git $INFER_HOME
+    # git clone --depth=50 https://github.com/facebook/infer.git $INFER_HOME
+    curl -o $HOME/infer-0.2.0.zip https://github.com/facebook/infer/archive/v0.2.0.zip
+    unzip $HOME/infer-0.2.0.zip -d $HOME/infer-0.2.0
+    ls $HOME/infer-0.2.0
+    ln -s $HOME/infer-0.2.0 $HOME/infer
+    ls $HOME/infer
 fi
 find $INFER_HOME -name infer
