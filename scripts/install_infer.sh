ad08cc8c1043f21dc499c8d98484bb21d47644fe
West Wang <west@misfit.com>
2015-07-05 15:33:25 +0800

    fix install infer

--- scripts/install_infer.sh
+++ scripts/install_infer.sh
@@ -6,7 +6,7 @@ mkdir -p $INFER_HOME
 cd $INFER_HOME
 
 if [[ ! -f infer/bin/infer ]]; then
-    curl -L -O https://github.com/facebook/infer/archive/v0.2.0.tar.gz
+    curl -L -o infer-0.2.0.zip https://github.com/facebook/infer/archive/v0.2.0.tar.gz
     unzip -q infer-0.2.0.zip
     mv infer-0.2.0 infer
     infer/update-fcp.sh
