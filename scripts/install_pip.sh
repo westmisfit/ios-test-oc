f8d6e40016106b803046b6e74b055e3271a3572a
West Wang <west@misfit.com>
2015-07-17 12:17:09 +0800

    install pip

--- scripts/install_pip.sh
+++ scripts/install_pip.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 set -ex
-which pip > /dev/null || curl -O https://bootstrap.pypa.io/get-pip.py && sudo python get-pip.py
+
+if ! which pip > /dev/null ; then
+    curl -O https://bootstrap.pypa.io/get-pip.py
+    sudo python get-pip.py
+fi
